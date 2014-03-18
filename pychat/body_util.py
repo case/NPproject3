a82585a7b3b9426786f6930aa83dbc575f760d33
Nathan West <Lucretiel@gmail.com>
2014-03-18 03:57:42 -0400

    Implemented chunked encoding in body_util.make_body

--- pychat/body_util.py
+++ pychat/body_util.py
@@ -4,16 +4,30 @@ Created on Mar 18, 2014
 @author: nathan
 '''
 
+from itertools import count
+
+CHUNK_SIZE = 999
+
 
 def make_sender_line(name):
+    '''
+    Create a formatted, encoded sender line
+    '''
     return 'FROM {name}\n'.format(name=name).encode('ascii')
 
 
 def make_body(body):
+    '''
+    Create an encoded body, with correct length prefixing
+    '''
     body = body.encode('ascii')
     if len(body) <= 99:
         yield '{size}\n'.format(size=len(body)).encode('ascii')
         yield body
     else:
-        # TODO: chunked encoding
-        raise NotImplementedError
+        body = memoryview(body)
+        for i in count(0, CHUNK_SIZE):
+            chunk = body[i:i + CHUNK_SIZE]
+            yield 'C{size}\n'.format(size=len(chunk)).encode('ascii')
+            yield chunk
+        yield b'C0'
